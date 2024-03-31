b83b0fe1f1908acd42353680c777608aaf8f9161
raffathamires <raffaela.monteiro@gmail.com>
2024-03-31 10:43:19 -0300

    Remocao do only

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -57,7 +57,7 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('.woocommerce-message').should('contain', qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
     });
 
-    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+    it('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
         cy.fixture('produtos').then(dados => {
             produtosPage.buscarProduto(dados[0].nomeProduto)
             produtosPage.addProdutoCarrinho(dados[0].tamanho,dados[0].cor,dados[0].quantidade)
